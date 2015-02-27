d98de0382c31584abe0eef0749f04b16a1c52a01
Adarsh Kosuru <adarsh.k09@gmail.com>
2015-02-27 16:47:54 -0500

    minor updates - tabspaces, indents

--- js/ViewportContainers.js
+++ js/ViewportContainers.js
@@ -5,19 +5,6 @@ var ViewportContainers = function ( editor ) {
 	var container = new UI.Panel();
 	container.setId( 'ViewportContainers' );
 
-	// container.add( new Sidebar.Forms( editor ) );
-	// container.add( new Sidebar.States( editor ) );
-
-	//container.add( new Sidebar.Properties( editor ) );
-
-	// container.add( new Sidebar.Renderer( editor ) );
-	// container.add( new Sidebar.Scene( editor ) );
-	// container.add( new Sidebar.Object3D( editor ) );
-	// container.add( new Sidebar.Geometry( editor ) );
-	// container.add( new Sidebar.Material( editor ) );
-	// container.add( new Sidebar.Animation( editor ) );
-	// container.add( new Sidebar.Script( editor ) );
-
 	return container;
 
 };
