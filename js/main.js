58d1d421703eff14d8bc8bd828e678149587d9be
Adarsh Kosuru <adarsh.k09@gmail.com>
2015-03-30 15:12:15 -0400

    Container drag n drop changes

--- js/main.js
+++ js/main.js
@@ -3,6 +3,86 @@
 
 			var editor = new Editor();
 
+						editor.signals.addInteractToContainer.add(function(cntr){
+						console.log(cntr);
+
+						interact('#'+cntr.id)
+						  .draggable({
+						    // enable inertial throwing
+						    inertia: true,
+						    // keep the element within the area of it's parent
+						    restrict: {
+						      restriction: "parent",
+						      endOnly: true,
+						      elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
+						    },
+
+						    // call this function on every dragmove event
+						    onmove: function (event) {
+						      var target = event.target,
+						          // keep the dragged position in the data-x/data-y attributes
+						          x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+						          y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+						      // translate the element
+						      target.style.webkitTransform =
+						      target.style.transform =
+						        'translate(' + x + 'px, ' + y + 'px)';
+
+						      // update the posiion attributes
+						      target.setAttribute('data-x', x);
+						      target.setAttribute('data-y', y);
+						    },
+						    // call this function on every dragend event
+						    onend: function (event) {
+						      var textEl = event.target.querySelector('p');
+
+						      textEl && (textEl.textContent =
+						        'moved a distance of '
+						        + (Math.sqrt(event.dx * event.dx +
+						                     event.dy * event.dy)|0) + 'px');
+						    }
+
+						  }).resizable({
+						    edges: { left: true, right: true, bottom: true, top: true }
+						  })
+						  .on('resizestart', function (event) {
+
+						    }).on('resizemove', function (event) {
+						    var target = event.target;
+
+						    // update the element's style
+						    target.style.width  = event.rect.width + 'px';
+						    target.style.height = event.rect.height + 'px';
+
+						    // translate when resizing from top or left edges
+						    offset.x += event.deltaRect.left;
+						    offset.y += event.deltaRect.top;
+
+						    target.style.transform = ('translate('
+						                              + offset.x + 'px,'
+						                              + offset.y + 'px)');
+
+						    // target.textContent = event.rect.width + '×' + event.rect.height;
+						  });
+
+			});
+
+			editor.signals.addDragToContainer.add(function(cntr) {
+				console.log(cntr);
+				interact('#'+cntr.id).dropzone({
+			          ondrop: function (event) {
+					console.log("ondropactivate");
+					console.log(event);
+			            console.log(event.relatedTarget.id);
+			              // editor.signals.elementDragnDrop.dispatch(event.relatedTarget.id);
+
+			            }
+
+			    });
+			});
+
+
 			var viewport = new Viewport( editor );
 			document.body.appendChild( viewport.dom );
 
--- js/views/FreeContainer.js
+++ js/views/FreeContainer.js
@@ -3,13 +3,41 @@
 
 var FreeContainer = function ( editor ) {
 
-	var container = new UI.Panel();
-	container.setId( 'freeContainer' );
+	var container = new UI.Container();
+
+	var icon = new UI.Cross();
+
+	icon.onClick(function() {
+		editor.signals.deleteFreeContainer.dispatch(this.dom.parentNode);
+
+	});
+	// container.dom.appendChild(icon.dom);
+	// container.setId( 'freeContainer' );
 
 	// container.setStyle('top: 50%',['top']]);
 	// container.setStyle('outline','1px dashed red',['outline']);
 
+	editor.signals.deleteFreeContainer.add(function(containerDelete) {
 
-	return container;
+		$("#"+containerDelete.id).remove();
+	});
+
+	var divContainer = document.createElement('div');
+	divContainer.appendChild(container.dom);
+	divContainer.className = "free-container";
+	divContainer.id = THREE.Math.generateUUID();
+	divContainer.appendChild(icon.dom);
+
+	return divContainer;
 
 };
+
+FreeContainer.prototype.delete = function(){
+
+
+
+};
+
+
+
+
